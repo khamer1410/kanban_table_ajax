339b5387e14e89ceb0000b19cc375a7842cfea98
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-02-05 21:41:18 +0100

    fix double card/column error

--- js/Board.js
+++ js/Board.js
@@ -8,18 +8,22 @@ var board = {
 };
 
 $('.create-column').click(function() {
-        var columnName = prompt('Wpisz nazwę kolumny');
-        $.ajax({
-    		url: baseUrl + '/column',
-    		method: 'POST',
-    		data: {
-            	name: columnName
-    		},
-    		success: function(response){
-    			var column = new Column(response.id, columnName);
-    			board.createColumn(column);
-          	}
-        });
+    var columnName = prompt('Wpisz nazwę kolumny');
+
+    if (columnName) {
+      $.ajax({
+    	url: baseUrl + '/column',
+    	method: 'POST',
+    	data: {
+          	name: columnName
+    	},
+    	success: function(response){
+    		var column = new Column(response.id, columnName);
+    		board.createColumn(column);
+        	}
+      });
+    }
+
 });
 	
 function initSortable() {
